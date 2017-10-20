0a66ef1ef6ff714f9b7fc6d0d2353b993e0f7f88
Adam Joyce <adamjoyce@outlook.com>
2017-10-20 14:45:35 +0100

    Covered forms

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
+        views.add_page, name='add_page'),
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -71,24 +71,24 @@ def add_category(request):
     # Render the form with error messages if any.
     return render(request, 'rango/add_category.html', {'form': form})
 
-def add_page(request):
+def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
         category = None
 
-        form = PageForm()
-        if request.method == 'POST':
-            form = PageForm(request.POST)
-            if form.is_valid():
-                if category:
-                    page = form.save(commit=False)
-                    page.category = category
-                    page.views = 0
-                    page.save()
-                    return show_category(request, category_name_slug)
-            else:
-                print(form.errors)
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
 
     context_dict = {'form':form, 'category': category}
     return render(request, 'rango/add_page.html', context_dict)
