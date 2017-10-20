229b37f31e2a01edbc9698198595a352cfa5bd8e
Adam Joyce <adamjoyce@outlook.com>
2017-10-20 16:40:27 +0100

    Covered maintainable templates and urls

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from rango import views
 
+app_name = 'rango'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
